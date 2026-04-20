9375bae685b8ef838ec3b9caa4ce2068341f149e
Mayank Tomar <128996500+tomar9897@users.noreply.github.com>
2026-04-20 07:15:53 +0530

    Enhance ChromeDriver setup with headless options
    Updated the getDriver method to use ChromeOptions for headless execution and added additional arguments for performance.

--- src/main/java/utils/DriverManager.java
+++ src/main/java/utils/DriverManager.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -19,14 +20,24 @@ public class DriverManager {
     private static WebDriver driver;
     
     public static WebDriver getDriver() {
-        if (driver == null) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        }
-        return driver;
+    if (driver == null) {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
+    return driver;
+}
     
     public static void quitDriver() {
         if (driver != null) {
