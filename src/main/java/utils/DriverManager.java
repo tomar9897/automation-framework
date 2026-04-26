27eff3b70c2aeab31ca5cea4084debe6a34b9e45
Mayank Tomar <128996500+tomar9897@users.noreply.github.com>
2026-04-26 16:27:33 +0530

    Add remote-allow-origins argument for Chrome options

--- src/main/java/utils/DriverManager.java
+++ src/main/java/utils/DriverManager.java
@@ -29,6 +29,7 @@ public class DriverManager {
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
 
         options.addArguments("--window-size=1920,1080");
 
